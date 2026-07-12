cffb76d56da0d0d0c5f118869276f7b29e930bca
zengyf006-ux <zengyf006@gmail.com>
2026-07-12 23:34:33 +0800

    feat: export persistent market cache

--- atlas-x-unified/packages/market-data/src/index.ts
+++ atlas-x-unified/packages/market-data/src/index.ts
@@ -2,4 +2,5 @@ export * from './types.js';
 export * from './connection.js';
 export * from './sequence.js';
 export * from './cache.js';
+export * from './indexeddb-cache.js';
 export * from './coinbase.js';
